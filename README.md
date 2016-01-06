c70300736350b97a7ed25e6754cd1d26e6debb97
Chris Marshall <chris@chrismar035.com>
2016-01-05 22:22:13 -0500

    Generate and print random puzzles and solutions

---
+++ README.md
@@ -0,0 +1,12 @@
+Sudoku Generator
+
+For Sudoku, this program generates:
+
+- [x] Random filled puzzle boards
+- [x] A puzzle for the filled board
+
+
+Easy has 46 blanks.
+Med has 51 blanks.
+Hard has 53 blanks.
+Evil has 55 blanks.
--- main.go
+++ main.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
-	"github.com/chrismar035/solver"
+	"github.com/chrismar035/sudoku-solver"
 )
+
+type removedSquare struct {
+	index int
+	value int
+}
 
 func main() {
 	var grid solver.Grid
-	puzzle := solver.Puzzle{Initial: grid}
-	solver := solver.NewRandBacktrackingSolver()
-	puzzle.Solution = solver.Solve(grid)
-	fmt.Println(puzzle)
+	randomizer := solver.NewRandBacktrackingSolver()
+
+	puzzle := randomizer.Solve(grid)
+	solution := puzzle
+	fmt.Println("Solution:")
+	fmt.Println(solution)
+
+	indexes := randomizeIndexes()
+	var removed []removedSquare
+	i := 0
+
+	solver := solver.NewMultiBacktrackingSolver()
+
+	for {
+		index := indexes[i]
+		removed = append(removed, removedSquare{index: index, value: puzzle[index]})
+		puzzle[index] = 0
+
+		if len(solver.Solve(puzzle)) > 1 {
+			last := removed[len(removed)-1]
+			puzzle[last.index] = last.value
+
+			fmt.Println("\nPuzzle:")
+			fmt.Println(puzzle)
+			return
+		}
+		i++
+		if i > 80 {
+			fmt.Println("Couldn't find puzzle")
+			return
+		}
+	}
+}
+
+func randomizeIndexes() []int {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	ints := []int{}
+	for i := 0; i < 81; i++ {
+		ints = append(ints, i)
+	}
+
+	mixed := []int{}
+	for len(ints) > 0 {
+		i := rand.Int() % len(ints)
+		mixed = append(mixed, ints[i])
+		ints = append(ints[0:i], ints[i+1:]...)
+	}
+
+	return mixed
 }
